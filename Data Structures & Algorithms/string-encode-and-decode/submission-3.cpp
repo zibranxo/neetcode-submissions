1ac3996e66134a329f1c8fd705950cfe59862695
arnav arnav <thearnav001@gmail.com>
2026-07-07 21:46:43 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans;
+        for(string s:strs){
+            ans+=to_string(s.size());
+            ans+='#';
+            ans+=s;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#') j++;
+
+            int len=stoi(s.substr(i, j-i));
+            ans.push_back(s.substr(j+1, len));
+            i=j+len+1;
+        }
+        return ans;
+    }
+};
