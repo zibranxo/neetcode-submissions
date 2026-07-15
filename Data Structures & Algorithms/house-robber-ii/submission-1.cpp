81adceeed76913647af52b7d3e42ab0fcdefb752
arnav <thearnav001@gmail.com>
2026-07-16 03:09:38 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(n + 2, 0), dp2(n+2, 0);
+        for (int i = n - 1; i >= 1; i--) {
+            int take = nums[i] + dp1[i + 2], skip = dp1[i + 1];
+            dp1[i] = max(take, skip);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            int take = nums[i] + dp2[i + 2], skip = dp2[i + 1];
+            dp2[i] = max(take, skip);
+        }
+        return max(dp1[1], dp2[0]);
+    }
+};
