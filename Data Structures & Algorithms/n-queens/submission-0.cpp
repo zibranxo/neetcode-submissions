ad984dedff7d4885a89cf0ba03ff0dd98d27cd94
arnav <thearnav001@gmail.com>
2026-07-24 09:13:07 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) if (board[i][col] == 'Q') return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) if (board[i][j] == 'Q') return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) if (board[i][j] == 'Q') return false;
+
+        return true;
+    }
+
+    void solve(int row, vector<string>& board, int n) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                solve(row + 1, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        solve(0, board, n);
+        return ans;
+    }
+};
